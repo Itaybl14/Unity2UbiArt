27bd8cb42d78524884e500e196b054cecf0d453c
Itaybl14 <84288727+Itaybl14@users.noreply.github.com>
2022-10-30 17:39:31 +0200

    fixed GoldEffectClips

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ print('Unity2Ubiart by Rama and Itay')
 
 def hextoubiart(hex):
     # hex2ubiart by WorteJD, source: https://github.com/WorteJD/HEX-to-UbiArt-Color
-    transparency = 100
     rgb = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
     output = []
-    output.append(str(float(transparency) / 100)[:8])
+    output.append(str(float(100) / 100)[:8])
     output.append(str(rgb[0] / 255)[:8])
     output.append(str(rgb[1] / 255)[:8])
     output.append(str(rgb[2] / 255)[:8])
@@ -39,29 +38,25 @@ for filename in os.listdir('input'):
     # DanceData Tape
     tape['Clips'] = []
     dancedata = mainjson['m_danceData']['MotionClips']
-    gestures = 0
-    msm = 0
+    GoldEffectClips = []
     for move in dancedata:
+        move['__class'] = 'MotionClip'
         move['Color'] = hextoubiart(move['Color'].replace('0xFF', '')) # for some reason it uses hex colors with 0xFF in the start just like Just Dance Now
         move['MotionPlatformSpecifics'] = {"X360":{"__class":"MotionPlatformSpecific","ScoreScale":1,"ScoreSmoothing":0,"LowThreshold":0.200000,"HighThreshold":1},"ORBIS":{"__class":"MotionPlatformSpecific","ScoreScale":1,"ScoreSmoothing":0,"LowThreshold":-0.200000,"HighThreshold":0.600000},"DURANGO":{"__class":"MotionPlatformSpecific","ScoreScale":1,"ScoreSmoothing":0,"LowThreshold":0.200000,"HighThreshold":1}}
-        move['__class'] = 'MotionClip'
-        if move['GoldMove'] == 1 and '.msm' in move['ClassifierPath']: # making Gold Move Effect
-            GoldEffectClip = {"__class":"GoldEffectClip","IsActive":1,"Duration":24,"EffectType":0}
-            GoldEffectClip['Id'] = randint(1000000000, 9999999999) # random
-            GoldEffectClip['TrackId'] = randint(1000000000, 9999999999)
-            GoldEffectClip['StartTime'] = move['StartTime'] + 12 # were adding 12 to the starttime unless it will come too early
-            tape['Clips'].append(GoldEffectClip)
+        if move['GoldMove'] == 1 and move['StartTime'] not in GoldEffectClips: # making Gold Move Effect
+            GoldEffectClips.append(move['StartTime']) # adding the StartTime to a list for not making multiple effects at the same time
+            # were adding 12 to the starttime unless it will come too early
+            tape['Clips'].append({"__class":"GoldEffectClip","IsActive":1,"Duration":24,"EffectType":0,"Id":randint(1000000000, 9999999999),"TrackId":randint(1000000000, 9999999999),"StartTime":move['StartTime'] + 12})
         if f'world/maps/{MapName.lower()}/timeline/moves/' not in move['ClassifierPath']:
-            newmove = f'world/maps/{MapName.lower()}/timeline/moves/{move["ClassifierPath"]}'
-            move['ClassifierPath'] = newmove
+            move['ClassifierPath'] = f'world/maps/{MapName.lower()}/timeline/moves/{move["ClassifierPath"]}'
         tape['Clips'].append(move)
 
-    pictoclips = mainjson['m_danceData']['PictoClips']
-    for picto in pictoclips:
+    for picto in mainjson['m_danceData']['PictoClips']:
         picto['PictoPath'] = f"world/maps/{MapName.lower()}/timeline/pictos/{picto['PictoPath'] + '.png'}" # your picto has to be named pictoname.png.ckd !!
         picto['__class'] = 'PictogramClip'
         tape['Clips'].append(picto)
-
+    
+    tape['Clips'].sort(key=lambda x: x["StartTime"]) # sorting the clips by their StartTime
     open(f'output/{MapName}/{MapName.lower()}_tml_dance.dtape.ckd', 'w', encoding="utf-8").write(json.dumps(tape))
 
     # MusicTrack
@@ -87,10 +82,10 @@ for filename in os.listdir('input'):
     # songDesc
     m_songDesc = mainjson['m_songDesc']
     NumCoach = m_songDesc['NumCoach']
-    cover = f"world/maps/{MapName.lower()}/menuart/textures/{MapName.lower()}_cover_phone.jpg"
-    songdesc = {"__class":"Actor_Template","WIP":0,"LOWUPDATE":0,"UPDATE_LAYER":0,"PROCEDURAL":0,"STARTPAUSED":0,"FORCEISENVIRONMENT":0,"COMPONENTS":[{"__class":"JD_SongDescTemplate","MapName":MapName,"JDVersion":m_songDesc['JDVersion'],"OriginalJDVersion":m_songDesc['OriginalJDVersion'],"Artist":m_songDesc['Artist'],"DancerName":"Unknown Dancer","Title":m_songDesc['Title'],"Credits":m_songDesc['Credits'],"PhoneImages":{"cover":cover},"NumCoach":NumCoach,"MainCoach":-1,"Difficulty":m_songDesc['Difficulty'],"SweatDifficulty":m_songDesc['SweatDifficulty'],"backgroundType":0,"LyricsType":0,"Tags":["main"],"Status":3,"LocaleID":4294967295,"MojoValue":0,"CountInProgression":1,"DefaultColors":{"lyrics":[1,1,0,0],"theme":[1,1,1,1]},"VideoPreviewPath":""}]}
+    songdesc = {"__class":"Actor_Template","WIP":0,"LOWUPDATE":0,"UPDATE_LAYER":0,"PROCEDURAL":0,"STARTPAUSED":0,"FORCEISENVIRONMENT":0,"COMPONENTS":[{"__class":"JD_SongDescTemplate","MapName":MapName,"JDVersion":m_songDesc['JDVersion'],"OriginalJDVersion":m_songDesc['OriginalJDVersion'],"Artist":m_songDesc['Artist'],"DancerName":"Unknown Dancer","Title":m_songDesc['Title'],"Credits":m_songDesc['Credits'],"PhoneImages":{"cover":f"world/maps/{MapName.lower()}/menuart/textures/{MapName.lower()}_cover_phone.jpg"},"NumCoach":NumCoach,"MainCoach":-1,"Difficulty":m_songDesc['Difficulty'],"SweatDifficulty":m_songDesc['SweatDifficulty'],"backgroundType":0,"LyricsType":0,"Tags":["main"],"Status":3,"LocaleID":4294967295,"MojoValue":0,"CountInProgression":1,"DefaultColors":{"lyrics":[1,1,0,0],"theme":[1,1,1,1]},"VideoPreviewPath":""}]}
     for i in range(NumCoach):
         i += 1
         songdesc['COMPONENTS'][0]['PhoneImages'][f'coach{i}'] = f"world/maps/{MapName.lower()}/menuart/textures/{MapName.lower()}_coach_{i}_phone.png"
 
     open(f'output/{MapName}/songdesc.tpl.ckd', 'w', encoding="utf-8").write(json.dumps(songdesc))
+    print(f"{m_songDesc['Title']} by {m_songDesc['Artist']}")
